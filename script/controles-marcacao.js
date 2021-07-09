e48e4b14fee0a5ce2b33dd86af3b8bc011b057e2
armonova <arthurnovaes0@gmail.com>
2021-07-08 21:42:20 -0300

    fix double code

--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -1,12 +1,3 @@
-
-// Exercício 0
-const ancestral = document.querySelector('body');
-const hiddenMark = document.querySelector('#visibilidade-das-marcacoes');
-
-hiddenMark.addEventListener('input', () => {
-    ancestral.classList.toggle(hiddenMark.value, hiddenMark.checked);
-})
-
 // Exercício 0
 const ancestral = document.querySelector('body');
 const hiddenMark = document.querySelector('#visibilidade-das-marcacoes');
